75c7dafd5ec19e305f42396d5da1f131e3cbc7d4
Jan Hoffmann <jan@hfmn.pl>
2022-01-30 19:19:22 +0100

    removed dist

--- client/dist/helpers.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sum = void 0;
-var sum = function (a, b) {
-    return a + b.length;
-};
-exports.sum = sum;
-//# sourceMappingURL=helpers.js.map
