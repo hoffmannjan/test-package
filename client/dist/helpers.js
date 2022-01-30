035430d31fa97199b95c4eefae1c45d68ba5441f
Jan Hoffmann <jan@hfmn.pl>
2022-01-30 19:16:02 +0100

    dist added

---
+++ client/dist/helpers.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sum = void 0;
+var sum = function (a, b) {
+    return a + b.length;
+};
+exports.sum = sum;
+//# sourceMappingURL=helpers.js.map
